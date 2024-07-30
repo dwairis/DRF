7c67aeacd2319cc288b104882746e473f93bb621
Ahmad Abu Fares <a.abufares@outlook.com>
2024-07-30 14:36:08 +0300

    js

--- DRF/Models/RequestUpdates.cs
+++ DRF/Models/RequestUpdates.cs
@@ -4,6 +4,7 @@
 public class RequestUpdates
 {
     [Dapper.Contrib.Extensions.ExplicitKey]
+    pub
     public int? RequestId { get; set; }
     public string Update { get; set; }
     public int? CreatedBy { get; set; }
